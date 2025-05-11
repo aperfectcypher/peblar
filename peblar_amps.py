5da53b46d5cd489101d9e0a3cb91598282d8a662
Langohr Thomas <tla@bea.be>
2025-05-11 23:44:25 +0200

    first commit

---
+++ peblar_amps.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+url = sys.argv[1]
+password = sys.argv[2]
+if len(sys.argv) != 3:
+    print("Usage: python peblar_amps.py <url> <password>")
+    sys.exit(1)
+    
+os.environ['MOZ_HEADLESS'] = '1'
+driver = webdriver.Firefox()
+driver.get(url)
+
+# Login
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.presence_of_element_located((By.ID, 'password')))
+pass_field = driver.find_element(By.ID, "password")
+pass_field.send_keys(password)
+
+button_xpath = "//button[contains(text(), 'Sign in')]"
+button = driver.find_element(By.XPATH, button_xpath)
+button.click()
+
+# Wait to find at least on current measurement (ending in ' A')
+table_xpath = "//div[contains(text(), ' A')]"
+element = wait.until(EC.presence_of_element_located((By.XPATH, table_xpath)))
+
+# get all current measurements
+current_measurements = driver.find_elements(By.XPATH, "//div[contains(text(), ' A')]")
+
+# Print the 3 fist current measurements
+for i in range(3):
+    print(current_measurements[i].text)
+
+driver.close()
